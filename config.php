a6684e2401e5267e352de19d77e9a3904ad74104
Filavandriel <hotkoyuki@gmail.com>
2018-02-03 17:02:36 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Define DB Params
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "db666");
+
+// Define URL
+define("ROOT_PATH", "/Sprintclean-master/");
+define("ROOT_URL", "http://localhost/Sprintclean-master/");
+//PATHS
+define("CSS_URL","/Sprintclean-master/css/");
+define("JS_PATH","/Sprintclean-master/js/");
+define("IMG_PATH","/Sprintclean-master/img/");
+define("CSS_URL2","/Sprintclean-master/views/wizualizacja/css/");
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// Start Session
+session_start();
+
+// Include Config
+require('config.php');
+
+require('classes/Bootstrap.php');
+require('classes/Controller.php');
+require('classes/Model.php');
+
+require('controllers/shares.php');
+require('controllers/users.php');
+require('controllers/pages.php');
+require('controllers/subpages.php');
+require('controllers/home.php');
+require('controllers/wizualizacja.php');
+
+
+require('models/share.php');
+require('models/user.php');
+require('models/pages.php');
+require('models/subpages.php');
+require('models/home.php');
+require('models/wizualizacja.php');
+
+$bootstrap = new Bootstrap($_GET);
+$controller = $bootstrap->createController();
+if($controller){
+	$controller->executeAction();
+}
